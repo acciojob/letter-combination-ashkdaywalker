c4d9601b4eb8e19133d07703e39d538196a82f04
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-01-20 13:52:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
+const mapping ={
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+  '2': 'abc',
+};
+
+
 function letterCombinations(input_digit) {
   //Complete the function
+  if(!digits) return []
+  let result = [''];
+  for(let i=0;i< digits.length;i++){
+    let currentDigit = digits[i];
+    let temp = []
+    for(let j=0;j< result.length;j++){
+      for(let k=0;k<mapping[currentDigit].length;k++){
+        temp.push(result[j]+ mapping[currentDigit][k]);
+      
+      }
+    }
+    result = temp;
+  }
+  return result.sort();
 }
 
 module.exports = letterCombinations;
